b13b990ddf3c6a5b3c0ec9414feb9ddfcf34ac4f
Hui Yee <huiyee.chong@techstudio.com.sg>
2018-06-29 11:17:06 +0800

    Peanut Framework

---
+++ Peanut/Peanut/Peanut.h
@@ -0,0 +1,19 @@
+//
+//  Peanut.h
+//  Peanut
+//
+//  Created by Shaylee Chong on 29/6/18.
+//  Copyright © 2018 ShayleeChong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Peanut.
+FOUNDATION_EXPORT double PeanutVersionNumber;
+
+//! Project version string for Peanut.
+FOUNDATION_EXPORT const unsigned char PeanutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Peanut/PublicHeader.h>
+
+
